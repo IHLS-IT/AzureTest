4757c1510366c70b9b1a13e91a424977948a3382
Dylan McDonald <dylan.mcdonald@inhouselegalsolutions.com>
2024-05-13 12:42:00 +0100

    Update Azure App Service workflow and add node_modules configurations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// const env = require('env');
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.use('/any', (req, res) => {
     res.send('This is a test route for any.');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
