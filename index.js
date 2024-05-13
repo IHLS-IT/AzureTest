25b67a89c5473f30e5dd0d032dca110ca483c6eb
Dylan McDonald <dylan.mcdonald@inhouselegalsolutions.com>
2024-05-13 16:50:57 +0100

    chore: Update index.js with ES6 module imports and add route for any.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const express = require('express');
-// const env = require('env');
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+import cors from "cors";
+import process from "process";
+
+
+import anyRoutes from "../routes/any.js";
+
 
 const app = express();
 
 
-app.get('/', (req, res) => {
-res.send('Hello, World! This is the root path.');
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", () => {
+  res.send("Hello, World! This is the root path.");
 });
 
+app.get("/any", anyRoutes);
 
-
-app.get('/test', (req, res) => {
-res.send('This is a test route.');
-});
-
-app.use('/any', (req, res) => {
-    res.send('This is a test route for any.');
-});
-app.use('/uss', (req, res) => {
-    res.send('This is a test route for uss.');
-});
 
 const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
